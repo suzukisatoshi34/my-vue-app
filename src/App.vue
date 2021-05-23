af931b31959301e9849be0170f8565da23e3ecbc
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-05-23 17:27:19 +0900

    TodoList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hello-wrold">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <div class="hello-wrold__message">
+        {{ message }}
+      </div>
+    </div>
+    <TodoList></TodoList>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
-
+import TodoList from "@/components/TodoList.vue"
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    TodoList,
   },
+  data: function () {
+    return {
+      message: "WebExpert Course Vue Template",
+    }
+  },
+  methods: {},
 }
 </script>
 
@@ -21,8 +31,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+</style>
+
+<style scoped>
+.hello-wrold {
   margin-top: 60px;
+  text-align: center;
+}
+
+.hello-wrold .hello-world__message {
+  color: #2c3e50;
 }
 </style>
